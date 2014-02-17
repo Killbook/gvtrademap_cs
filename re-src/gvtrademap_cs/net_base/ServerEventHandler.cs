367accef5946bebd0cb7a27a938e76368cae6fd4
xia <xia@silvia.com>
2014-02-17 19:55:58 +0900

    不要ファイル削除

--- re-src/gvtrademap_cs/net_base/ServerEventHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿// Type: net_base.ServerEventHandler
-// Assembly: gvtrademap_cs, Version=1.3.2.3, Culture=neutral, PublicKeyToken=null
-// MVID: 3D162A44-1A8B-4B7A-9FC3-6379559CB419
-// Assembly location: C:\tmp\A\files\gvtrademap_cs.exe
-
-namespace net_base
-{
-  public delegate void ServerEventHandler(object sender, ServerEventArgs e);
-}
